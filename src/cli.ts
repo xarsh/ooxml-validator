1773ecdfd697bfebec436052f3dd1ad12da31c64
Takao Naito <xarsh@outlook.com>
2025-12-04 15:01:29 +0900

    Remove commander deps.

--- src/cli.ts
+++ src/cli.ts
@@ -1,18 +1,28 @@
 #!/usr/bin/env node
-import { program } from 'commander'
+import { parseArgs } from 'node:util'
 import { type RunOptions, validateFile } from './index.js'
 
-program.name('ooxml-validator').argument('<file>', 'OOXML file to validate').option('--office-version <version>', 'Office version', 'Microsoft365').parse(process.argv)
+const { values, positionals } = parseArgs({
+	options: {
+		officeVersion: {
+			type: 'string',
+		},
+	},
+	allowPositionals: true,
+})
 
-const opts = program.opts<{ officeVersion: string }>()
-const file = program.args[0]
+if (positionals.length === 0) {
+	console.error('Usage: ooxml-validator <file> [--office-version <version>]')
+	process.exit(2)
+}
 
-validateFile(file, { officeVersion: opts.officeVersion as RunOptions['officeVersion'] })
+const officeVersion = values.officeVersion ?? 'Microsoft365'
+const file = positionals[0]
+
+validateFile(file, { officeVersion: officeVersion as RunOptions['officeVersion'] })
 	.then((res) => {
 		console.log(JSON.stringify(res, null, 2))
-
 		const ok = (res.ok as boolean | undefined) ?? false
-
 		process.exit(ok ? 0 : 1)
 	})
 	.catch((err) => {
